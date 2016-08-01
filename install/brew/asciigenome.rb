ad6d67f09776b58b182b48fc48110518629adf46
Giovanni Dall'Olio <giovanni.m.dallolio@gsk.com>
2016-08-01 12:31:23 -0400

    BREW: updated formula to 0.2.0

--- install/brew/asciigenome.rb
+++ install/brew/asciigenome.rb
@@ -2,7 +2,8 @@ class Asciigenome < Formula
   desc "Text Only Genome Viewer!"
   homepage "https://github.com/dariober/ASCIIGenome"
   url "https://github.com/dariober/ASCIIGenome/releases/download/v0.1.0/ASCIIGenome-0.2.0.zip"
-  sha256 "b4acfe7c474b79cf0640c32e9fc8b2637d2ede4ec44a669316357d2aa90fe9a3"
+  version "0.2.0"
+  sha256 "8547957b931acf10905579f81e553fc5e90e3365101e29a4ef8352c5626665aa"
 
   def install
     jar = "ASCIIGenome.jar"
