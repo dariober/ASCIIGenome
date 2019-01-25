3dd907f5d3d9034b8a8ff01fa0c58dd3bf04dcb0
Dario Beraldi <dario.beraldi@gmail.com>
2019-01-25 10:25:45 +0000

    Update brew

--- install/brew/asciigenome.rb
+++ install/brew/asciigenome.rb
@@ -1,8 +1,8 @@
 class Asciigenome < Formula
   desc "Text Only Genome Viewer!"
   homepage "https://github.com/dariober/ASCIIGenome"
-  url "https://github.com/dariober/ASCIIGenome/releases/download/v1.14.0/ASCIIGenome-1.14.0.zip"
-  sha256 "2ff6e9d2619d259dc833cd8fc0448240db95393f9bb52098c463a6f13e92df27"
+  url "https://github.com/dariober/ASCIIGenome/releases/download/v1.15.0/ASCIIGenome-1.15.0.zip"
+  sha256 "7b70ac0c7b76f704e3e9eb18bb5f42ff734e598e3882228f5a195a19f9b347c0"
 
   def install
     jar = "ASCIIGenome.jar"
